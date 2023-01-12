4f8c9fecdf0e5c2dd8bbd1f8a5f6ab878347ee33
orcooo <114367118+orcooo@users.noreply.github.com>
2023-01-13 01:44:50 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+
 cd /www/server/panel/install
 wget -N https://github.com/orcooo/BTpanel-arm-LuajIT/releases/download/nginx-deploy/nginx-configuration.zip
+unzip nginx-configuration.zip
+rm nginx-configuration.zip
